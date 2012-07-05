ff568ee196a7807aef15d1e883ee0d5f145805cf
Daichi Suzuo <d103726@edu.imc.tut.ac.jp>
2012-07-06 00:38:43 +0900

    それぞれの統計項目の値を計算して出力できるようにした

--- acc.cpp
+++ acc.cpp
@@ -27,6 +27,23 @@ int main(int argc, char* argv[])
     if(cmdline_parser(argc, argv, &arg))
         return -1;
 
-    std::cout << accumulate<double, acc::tag::sum>() << std::endl;;
+    if(!arg.integer_given)
+    {
+        if(arg.sum_given)  accumulate<double, acc::tag::sum>();
+        if(arg.min_given)  accumulate<double, acc::tag::min>();
+        if(arg.max_given)  accumulate<double, acc::tag::max>();
+        if(arg.sum_given)  accumulate<double, acc::tag::sum>();
+        if(arg.mean_given) accumulate<double, acc::tag::mean>();
+        if(arg.var_given)  accumulate<double, acc::tag::variance>();
+        if(arg.med_given)  accumulate<double, acc::tag::median>();
+    }else{
+        if(arg.sum_given)  accumulate<int, acc::tag::sum>();
+        if(arg.min_given)  accumulate<int, acc::tag::min>();
+        if(arg.max_given)  accumulate<int, acc::tag::max>();
+        if(arg.sum_given)  accumulate<int, acc::tag::sum>();
+        if(arg.mean_given) accumulate<int, acc::tag::mean>();
+        if(arg.var_given)  accumulate<int, acc::tag::variance>();
+        if(arg.med_given)  accumulate<int, acc::tag::median>();
+    }
 }
 
