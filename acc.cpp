92dcd64e494c1c9e3dcfe6683f72fc168c8f4156
Daichi Suzuo <d103726@edu.imc.tut.ac.jp>
2012-07-07 00:55:14 +0900

    数値以外の入力に対してのエラー出力に対応

--- acc.cpp
+++ acc.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 #include <boost/accumulators/accumulators.hpp>
 #include <boost/accumulators/statistics.hpp>
+#include <boost/lexical_cast.hpp>
 #include <boost/format.hpp>
 
 #include "cmdline.h"
 
 namespace acc = boost::accumulators;
 
-
 template<class TFeature>
 double accumulate()
 {
     acc::accumulator_set<double, acc::features<TFeature>> accumulator;
 
-    double t;
-    while(std::cin >> t, !std::cin.eof())
-        accumulator(t);
+    std::string t;
+    try
+    {
+        while(std::cin >> t, !std::cin.eof())
+            accumulator(boost::lexical_cast<double>(t));
+    }catch(boost::bad_lexical_cast e)
+    {
+        throw t;
+    }
 
     return acc::extract_result<TFeature>(accumulator);
 }
@@ -28,12 +34,25 @@ int main(int argc, char* argv[])
         return -1;
 
     double t;
-    if(arg.sum_given)  t = accumulate<acc::tag::sum>();
-    if(arg.min_given)  t = accumulate<acc::tag::min>();
-    if(arg.max_given)  t = accumulate<acc::tag::max>();
-    if(arg.mean_given) t = accumulate<acc::tag::mean>();
-    if(arg.var_given)  t = accumulate<acc::tag::variance>();
-    if(arg.med_given)  t = accumulate<acc::tag::median>();
+    try
+    {
+        if(arg.sum_given)  t = accumulate<acc::tag::sum>();
+        if(arg.min_given)  t = accumulate<acc::tag::min>();
+        if(arg.max_given)  t = accumulate<acc::tag::max>();
+        if(arg.mean_given) t = accumulate<acc::tag::mean>();
+        if(arg.var_given)  t = accumulate<acc::tag::variance>();
+        if(arg.med_given)  t = accumulate<acc::tag::median>();
+    }
+    catch(const std::string& e)
+    {
+        std::cerr << argv[0] << ": \"" << e << "\" couldn't be converted into a number" << std::endl;
+        return -1;
+    }
+    catch(...)
+    {
+        std::cerr << argv[0] << ": unknown error" << std::endl;
+        return -1;
+    }
 
     std::cout << boost::format(arg.format_arg) % t << std::endl;
 }
